a0497d26ecb7f19990336302dac5ca6a34848a71
Petermwansa <mwansapeter800@gmail.com>
2024-02-10 23:57:08 +0300

    Link the JS and create ids for the elements

---
+++ index.js
@@ -0,0 +1,4 @@
+const searchForm = document.getElementById("search__form");
+const searchBox = document.getElementById("search__box");
+const searchResult = document.getElementById("search__result");
+const showMore = document.getElementById("show__more__btn");
